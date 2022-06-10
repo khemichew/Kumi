a933f954895c81467e30331515ccbb11cb5962a5
Khemi <khemi.chew20@imperial.ac.uk>
2022-06-10 02:47:08 +0100

    [feat] add create retail entry operation feature from app to database

--- app/lib/database_showcase.dart
+++ app/lib/database_showcase.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 // import 'package:app/style.dart';
 
 @immutable
@@ -62,72 +63,69 @@ class RetailerListState extends State<RetailerList> {
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        appBar: AppBar(
-          title: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              Text('Sort by'),
-            ],
-          ),
-          actions: [
-            PopupMenuButton<RetailerQuery>(
-              onSelected: (value) => setState(() => query = value),
-              icon: const Icon(Icons.sort),
-              itemBuilder: (BuildContext context) {
-                return [
-                  const PopupMenuItem(
-                    value: RetailerQuery.nameAsc,
-                    child: Text("Name ⬆"),
-                  ),
-                  const PopupMenuItem(
-                    value: RetailerQuery.nameDesc,
-                    child: Text("Name ⬇"),
-                  ),
-                  const PopupMenuItem(
-                    value: RetailerQuery.ratingAsc,
-                    child: Text("Rating ⬆"),
-                  ),
-                  const PopupMenuItem(
-                    value: RetailerQuery.ratingDesc,
-                    child: Text("Rating ⬇"),
-                  ),
-                ];
-              },
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            Text('Sort by'),
           ],
         ),
-        // Display query results
-        body:
-            StreamBuilder<QuerySnapshot<Retailer>>(
-              stream: retailerData.queryBy(query).snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Center(child: Text(snapshot.error.toString()));
-                }
+        actions: [
+          PopupMenuButton<RetailerQuery>(
+            onSelected: (value) => setState(() => query = value),
+            icon: const Icon(Icons.sort),
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(
+                  value: RetailerQuery.nameAsc,
+                  child: Text("Name ⬆"),
+                ),
+                const PopupMenuItem(
+                  value: RetailerQuery.nameDesc,
+                  child: Text("Name ⬇"),
+                ),
+                const PopupMenuItem(
+                  value: RetailerQuery.ratingAsc,
+                  child: Text("Rating ⬆"),
+                ),
+                const PopupMenuItem(
+                  value: RetailerQuery.ratingDesc,
+                  child: Text("Rating ⬇"),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      // Display query results
+      body: StreamBuilder<QuerySnapshot<Retailer>>(
+        stream: retailerData.queryBy(query).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
 
-                if (!snapshot.hasData) {
-                  return const Center(child: CircularProgressIndicator());
-                }
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-                final data = snapshot.requireData;
+          final data = snapshot.requireData;
 
-                // Create list of retailer entries
-                return ListView.builder(
-                  itemCount: min(data.size, displayLimit),
-                  itemBuilder: (context, index) {
-                    return _RetailerItem(
-                      data.docs[index].data(),
-                      data.docs[index].reference,
-                    );
-                  },
-                );
-              },
-            ),
-
-      );
+          // Create list of retailer entries
+          return ListView.builder(
+            itemCount: min(data.size, displayLimit),
+            itemBuilder: (context, index) {
+              return _RetailerItem(
+                data.docs[index].data(),
+                data.docs[index].reference,
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
 
@@ -169,12 +167,115 @@ class _RetailerItem extends StatelessWidget {
   }
 }
 
+class AddRetailer extends StatefulWidget {
+  const AddRetailer({super.key});
+
+  @override
+  AddRetailerState createState() {
+    return AddRetailerState();
+  }
+}
+
+class AddRetailerState extends State<AddRetailer> {
+  late String name;
+  late String description;
+  late int rating;
+  final _formKey = GlobalKey<FormState>();
+
+  Widget _displayForm(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              onSaved: (String? value) {
+                name = value!;
+              },
+              decoration: const InputDecoration(labelText: "Name"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              onSaved: (String? value) {
+                description = value!;
+              },
+              decoration: const InputDecoration(labelText: "Description"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+                onSaved: (String? value) {
+                  rating = int.parse(value!);
+                },
+                decoration: const InputDecoration(labelText: "Rating"),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              child: const Text("Submit"),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState?.save();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Entry added!')),
+                  );
+                  // save to database
+                  retailerData.add(Retailer(
+                      name: name, description: description, rating: rating));
+                }
+              },
+            ),
+          )
+        ]));
+  }
+
+  // Show dialog for user to fill in retailer details
+
+  Widget _addEntryButton(BuildContext context) {
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: ElevatedButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(content: _displayForm(context));
+                  });
+            },
+            child: const Text('Button', style: TextStyle(fontSize: 20))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _addEntryButton(context);
+  }
+}
+
 // Entry point.
 class DatabaseShowcase extends StatelessWidget {
   const DatabaseShowcase({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const RetailerList();
+    return Stack(children: const [RetailerList(), AddRetailer()]);
   }
 }
