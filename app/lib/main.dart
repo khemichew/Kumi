9356215b242b5524715d95123b85bdd2b3db325b
Max Meng <max.meng20@imperial.ac.uk>
2022-06-13 14:53:49 +0100

    Set default font to Overpass

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:app/style.dart';
+import 'package:app/memberships.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: _title,
       home: MyHomePage(title: _title),
+      theme: ThemeData(fontFamily: 'Overpass'),
     );
   }
 }
@@ -143,112 +145,6 @@ class AccountPage extends StatelessWidget {
                 ]
             ),
           ]
-      ),
-    );
-  }
-}
-
-class MembershipPage extends StatelessWidget {
-  const MembershipPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(15, 50, 15, 0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
-                    child: const Text(
-                      'My\nmemberships',
-                      style: titleStyle,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child:TextButton(
-                    onPressed: () {  },
-                    child: Container(
-                      height: 60,
-                      decoration:BoxDecoration(
-                          border: Border.all(
-                              color: Colors.black
-                          ),
-                          color: Colors.white38,
-                          borderRadius: const BorderRadius.all(Radius.circular(10))
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text("Add", style: optionStyle),
-                          Icon(Icons.add, color: Colors.black,)
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ]
-          ),
-          // SizedBox(height: 40,),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                MembershipCard(storeName: 'Store A', color: Color.fromRGBO(255, 191, 0, 0.5),),
-                MembershipCard(storeName: 'Store B', color: Color.fromRGBO(248, 152, 128, 0.5),),
-              ]
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                MembershipCard(storeName: 'Store C', color: Color.fromRGBO(137, 207, 240, 0.5),),
-                MembershipCard(storeName: 'Store D', color: Color.fromRGBO(115, 113, 255, 0.5),),
-              ]
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MembershipCard extends StatelessWidget {
-  const MembershipCard({Key? key, required this.storeName, required this.color}) : super(key: key);
-
-  final String storeName;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: TextButton(
-        onPressed: () {  },
-        child: Container(
-          height: 100,
-          decoration:BoxDecoration(
-              color: color,
-              borderRadius: const BorderRadius.all(Radius.circular(10))
-          ),
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(storeName, style: ordinaryStyle),
-                    const Icon(Icons.chevron_right, color: Colors.black,)
-                  ]
-              ),
-              // const Icon(Icons.card_membership)
-            ],
-          ),
-        ),
       ),
     );
   }
