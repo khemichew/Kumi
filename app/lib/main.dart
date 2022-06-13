0787fab29bc6284548e32d954fbc32d666412f54
Khemi <khemi.chew20@imperial.ac.uk>
2022-06-13 18:56:37 +0100

    [init] initial setup to do Account page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -144,8 +144,43 @@ class AccountPage extends StatelessWidget {
                   ),
                 ]
             ),
+            Row(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                PersonalDetail(field: "Name", entry: "Jim Brown")
+              ],
+            )
           ]
       ),
+    );
+  }
+}
+
+class PersonalDetail extends StatelessWidget {
+  const PersonalDetail({Key? key, required this.field, required this.entry}) : super(key: key);
+
+  final String field;
+  final String entry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+          height: 100,
+          decoration:const BoxDecoration(
+              color: Color.fromRGBO(0,0,0,0),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+                    Text(field, style: titleStyle),
+                    Text(entry, style: ordinaryStyle),
+            ],
+          ),
+        ),
     );
   }
 }
