fdf8d331598b2e3cc41df4f9d52deb292efcd2d7
Khemi <khemi.chew20@imperial.ac.uk>
2022-06-14 17:42:56 +0100

    [config] move files to lib directory

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:app/database_showcase.dart';
-import 'package:app/pages/explore.dart';
+import 'package:app/explore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:app/style.dart';
-import 'package:app/pages/memberships.dart';
+import 'package:app/memberships.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
