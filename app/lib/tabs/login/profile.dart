f354bab3c7d6564f99ae7aa5398d9240f48ac2db
mm2320 <mm2320@ic.ac.uk>
2022-06-22 15:45:58 +0100

    [feature] add profile page

--- app/lib/tabs/login/profile.dart
+++ app/lib/tabs/login/profile.dart
@@ -29,95 +29,176 @@ class ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'NAME: ${_currentUser.displayName}',
-          style: Theme.of(context).textTheme.bodyText1,
-        ),
-        quadSpacing,
-        Text(
-          'EMAIL: ${_currentUser.email}',
-          style: Theme.of(context).textTheme.bodyText1,
-        ),
-        quadSpacing,
-        _currentUser.emailVerified
-            ? Text(
-          'Email verified',
-          style: Theme.of(context)
-              .textTheme
-              .bodyText1!
-              .copyWith(color: Colors.green),
-        )
-            : Text(
-          'Email not verified',
-          style: Theme.of(context)
-              .textTheme
-              .bodyText1!
-              .copyWith(color: Colors.red),
-        ),
-        quadSpacing,
-        _isSendingVerification
-            ? const CircularProgressIndicator()
-            : Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  _isSendingVerification = true;
-                });
-                await _currentUser.sendEmailVerification();
-                setState(() {
-                  _isSendingVerification = false;
-                });
-              },
-              child: const Text('Verify email'),
-            ),
-            const SizedBox(width: 8.0),
-            TextButton(
-              child: const Icon(Icons.refresh),
-              onPressed: () async {
-                User? user = await FireAuth.refreshUser(_currentUser);
-
-                if (user != null) {
-                  setState(() {
-                    _currentUser = user;
-                  });
-                }
-              },
-            ),
-          ],
-        ),
-        quadSpacing,
-        _isSigningOut
-            ? const CircularProgressIndicator()
-            : ElevatedButton(
-          onPressed: () async {
-            setState(() {
-              _isSigningOut = true;
-            });
-            await FirebaseAuth.instance.signOut();
-            setState(() {
-              _isSigningOut = false;
-            });
-            // ignore: use_build_context_synchronously
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (context) => const LoginPage(),
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 60, 20, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'My\naccount',
+                style: largeTitleStyle,
               ),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            primary: Colors.red,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
+              // Image(image: AssetImage('../asset/dingzhen_cute.jpeg'))
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/dingzhen_cute.jpeg'),
+                radius: 60,
+              )
+            ]
           ),
-          child: const Text('Sign out'),
-        ),
-      ],
+          halfSpacing,
+          Align(
+            alignment: Alignment.topLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Name",
+                  style: emphStyle,
+                ),
+                Text(
+                  'Jim Brown',
+                  // '${_currentUser.displayName}',
+                  style: ordinaryStyle,
+                ),
+              ]
+            )
+          ),
+          halfSpacing,
+          Align(
+            alignment: Alignment.topLeft,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Email Address",
+                    style: emphStyle,
+                  ),
+                  Text(
+                    '${_currentUser.email}',
+                    style: ordinaryStyle,
+                  ),
+                ]
+            )
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: _currentUser.emailVerified
+              ? quadSpacing
+              : Text(
+                  'Email not verified',
+                  style: smallStyle .copyWith(color: Colors.red),
+                ),
+          ),
+          halfSpacing,
+          _isSendingVerification
+              ? const CircularProgressIndicator()
+              : SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            _isSendingVerification = true;
+                          });
+                          await _currentUser.sendEmailVerification();
+                          setState(() {
+                            _isSendingVerification = false;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: regularRadius,
+                          ),
+                          padding: allSidesTenInsets,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Verify email',
+                              style: ordinaryStyle.copyWith(color: Colors.white),
+                            ),
+                            halfSpacing,
+                            const Icon(Icons.email_outlined)
+                          ]
+                        )
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          User? user = await FireAuth.refreshUser(_currentUser);
+
+                          if (user != null) {
+                            setState(() {
+                              _currentUser = user;
+                            });
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.indigoAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: regularRadius,
+                          ),
+                          padding: allSidesTenInsets,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Refresh',
+                              style: ordinaryStyle.copyWith(color: Colors.white),
+                            ),
+                            halfSpacing,
+                            const Icon(Icons.refresh)
+                          ]
+                        )
+                      ),
+                    ],
+                  ),
+              ),
+          quadSpacing,
+          _isSigningOut
+              ? const CircularProgressIndicator()
+              : SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _isSigningOut = true;
+                      });
+                      await FirebaseAuth.instance.signOut();
+                      setState(() {
+                        _isSigningOut = false;
+                      });
+                      // ignore: use_build_context_synchronously
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: regularRadius,
+                      ),
+                      padding: allSidesTenInsets,
+                    ),
+                    child: Text(
+                      'Sign out',
+                      style: ordinaryStyle.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+        ],
+      )
     );
   }
 }
